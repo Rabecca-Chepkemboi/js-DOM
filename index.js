e43caf0240c3963322e1ce6f9098521f880dd5d4
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-05-24 16:25:34 +0300

    done js DOM

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('body').style.backgroundColor = 'silver';
+
+document.getElementById('title').style.color = 'green';
+
+document.getElementById('grocery').style.textTransform = 'uppercase';
+
+document.getElementById('groceries').style.textTransform = 'uppercase';
+
+
+let li = document.createElement('li');
+li.innerHTML = " Spinach";
+document.getElementById('vegList').appendChild(li)
+
+let fruList = document.createElement('ul');
+ul.innerHTML = " Oranges";
+document.getElementById('fruList').appendChild(ul)
+
